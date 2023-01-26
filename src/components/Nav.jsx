cb7c5880a36cf7e4a0786555a472ecbc30f2d9a0
tandica <williams.tandica@gmail.com>
2023-01-25 22:04:06 -0500

    style navbar and add links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,27 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Nav.scss";
+import logo from "../assets/ballersonly-logo.png";
 
 export default function Nav() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/stats">Stats</Link>
-        </li>
-        <li>
-          <Link to="/players">Players</Link>
-        </li>
-        <li>
-          <Link to="/teams">Teams</Link>
-        </li>
-        <li>
-          <Link to="/games">Games</Link>
-        </li>
-      </ul>
+    <nav className="nav-container">
+      <div className="nav-logo">
+        <img src={logo} alt="BallersOnly logo" />
+      </div>
+      <div className="nav-items">
+        <Link to="/">Home</Link>
+        <Link to="/stats">Stats</Link>
+        <Link to="/players">Players</Link>
+        <Link to="/teams">Teams</Link>
+        <Link to="/games">Games</Link>
+      </div>
     </nav>
   );
 }
