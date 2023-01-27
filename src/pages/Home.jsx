754d244cb805d229eca03a90f02578c73604d81e
tandica <williams.tandica@gmail.com>
2023-01-26 21:41:24 -0500

    style navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Nav from "../components/Nav";
-// import "../styles/Nav.scss";
 
 export default function Home() {
   return (
