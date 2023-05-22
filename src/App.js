637377be5821b18e21034e7030c28a056a9f31e9
tandica <williams.tandica@gmail.com>
2023-05-21 21:52:22 -0400

    create pages for player, games, tems and stats

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./styles/App.css";
 // import Nav from "./components/Nav";
 import Home from "./pages/Home";
+import Stats from "./pages/Stats";
+import Players from "./pages/Players";
+import Teams from "./pages/Teams";
+import Games from "./pages/Games";
 
 function App() {
   return (
@@ -13,6 +17,10 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/stats" element={<Stats />} />
+        <Route path="/players" element={<Players />} />
+        <Route path="/teams" element={<Teams />} />
+        <Route path="/games" element={<Games />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Players.jsx
@@ -0,0 +1,11 @@
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
+
+export default function Players() {
+  return (
+    <div>
+      <Nav />
+      <Footer />
+    </div>
+  );
+}
